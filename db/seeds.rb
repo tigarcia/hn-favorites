c615e415e7be990cb7278475e2374c2e32191cea
Tim Garcia <tim.m.garcia@gmail.com>
2016-10-28 15:33:31 -0700

    Adding seeds for ray and greg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({email: "gkmarsella@gmail.com", password: "password1234", password_confirmation: "password1234"})
+User.create({email: "ravingrabbit75@gmail.com", password: "password1234", password_confirmation: "password1234"})
